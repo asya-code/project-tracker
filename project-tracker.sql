7b677fb940b12f48ccaa470c8e302fbec2f219b4
asya-code <77514066+asya-code@users.noreply.github.com>
2022-03-08 12:50:27 -0800

    worked on view

--- project-tracker.sql
+++ project-tracker.sql
@@ -21,12 +21,48 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: grades; Type: TABLE; Schema: public; Owner: alexandraskachkova
+--
+
+CREATE TABLE public.grades (
+    id integer NOT NULL,
+    student_github character varying(30),
+    project_title character varying(30),
+    grade integer
+);
+
+
+ALTER TABLE public.grades OWNER TO alexandraskachkova;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE; Schema: public; Owner: alexandraskachkova
+--
+
+CREATE SEQUENCE public.grades_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.grades_id_seq OWNER TO alexandraskachkova;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: alexandraskachkova
+--
+
+ALTER SEQUENCE public.grades_id_seq OWNED BY public.grades.id;
+
+
+--
 -- Name: projects; Type: TABLE; Schema: public; Owner: alexandraskachkova
 --
 
 CREATE TABLE public.projects (
     id integer NOT NULL,
-    title character varying(30),
+    title character varying(30) NOT NULL,
     description text,
     max_grade integer
 );
@@ -64,11 +100,28 @@ CREATE TABLE public.students (
     id integer NOT NULL,
     first_name character varying(30),
     last_name character varying(30),
-    github character varying(30)
+    github character varying(30) NOT NULL
 );
 
 
 ALTER TABLE public.students OWNER TO alexandraskachkova;
+
+--
+-- Name: report_card_view; Type: VIEW; Schema: public; Owner: alexandraskachkova
+--
+
+CREATE VIEW public.report_card_view AS
+ SELECT students.first_name,
+    students.last_name,
+    projects.title,
+    grades.grade,
+    projects.max_grade
+   FROM ((public.students
+     JOIN public.grades ON (((students.github)::text = (grades.student_github)::text)))
+     JOIN public.projects ON (((projects.title)::text = (grades.project_title)::text)));
+
+
+ALTER TABLE public.report_card_view OWNER TO alexandraskachkova;
 
 --
 -- Name: students_id_seq; Type: SEQUENCE; Schema: public; Owner: alexandraskachkova
@@ -93,6 +146,13 @@ ALTER SEQUENCE public.students_id_seq OWNED BY public.students.id;
 
 
 --
+-- Name: grades id; Type: DEFAULT; Schema: public; Owner: alexandraskachkova
+--
+
+ALTER TABLE ONLY public.grades ALTER COLUMN id SET DEFAULT nextval('public.grades_id_seq'::regclass);
+
+
+--
 -- Name: projects id; Type: DEFAULT; Schema: public; Owner: alexandraskachkova
 --
 
@@ -107,11 +167,28 @@ ALTER TABLE ONLY public.students ALTER COLUMN id SET DEFAULT nextval('public.stu
 
 
 --
+-- Data for Name: grades; Type: TABLE DATA; Schema: public; Owner: alexandraskachkova
+--
+
+COPY public.grades (id, student_github, project_title, grade) FROM stdin;
+1	jhacks	Markov	10
+2	jhacks	Blockly	2
+3	sdevelops	Markov	50
+4	sdevelops	Blockly	100
+5	asya-code	Markov	65
+6	asya-code	Blockly	20
+7	hoai-an-phan	Markov	70
+8	hoai-an-phan	Blockly	80
+\.
+
+
+--
 -- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: alexandraskachkova
 --
 
 COPY public.projects (id, title, description, max_grade) FROM stdin;
 1	Markov	Tweets generated from Markox chains	50
+2	Blockly	Programmatic Logic Puzzle Game	100
 \.
 
 
@@ -122,21 +199,38 @@ COPY public.projects (id, title, description, max_grade) FROM stdin;
 COPY public.students (id, first_name, last_name, github) FROM stdin;
 1	Jane	Hacker	jhacks
 2	Sarah	Developer	sdevelops
+3	An	Phan	hoai-an-phan
+4	Asya	Skachkova	asya-code
 \.
+
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE SET; Schema: public; Owner: alexandraskachkova
+--
+
+SELECT pg_catalog.setval('public.grades_id_seq', 8, true);
 
 
 --
 -- Name: projects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: alexandraskachkova
 --
 
-SELECT pg_catalog.setval('public.projects_id_seq', 1, true);
+SELECT pg_catalog.setval('public.projects_id_seq', 2, true);
 
 
 --
 -- Name: students_id_seq; Type: SEQUENCE SET; Schema: public; Owner: alexandraskachkova
 --
 
-SELECT pg_catalog.setval('public.students_id_seq', 2, true);
+SELECT pg_catalog.setval('public.students_id_seq', 4, true);
+
+
+--
+-- Name: grades grades_pkey; Type: CONSTRAINT; Schema: public; Owner: alexandraskachkova
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_pkey PRIMARY KEY (id);
 
 
 --
@@ -144,7 +238,7 @@ SELECT pg_catalog.setval('public.students_id_seq', 2, true);
 --
 
 ALTER TABLE ONLY public.projects
-    ADD CONSTRAINT projects_pkey PRIMARY KEY (id);
+    ADD CONSTRAINT projects_pkey PRIMARY KEY (title);
 
 
 --
@@ -152,7 +246,7 @@ ALTER TABLE ONLY public.projects
 --
 
 ALTER TABLE ONLY public.students
-    ADD CONSTRAINT students_pkey PRIMARY KEY (id);
+    ADD CONSTRAINT students_pkey PRIMARY KEY (github);
 
 
 --
